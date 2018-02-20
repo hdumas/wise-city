265de12278318d4baeb8f648a7e6d8f5738da10a
Henri <Henri@Henris-MacBook-Pro.local>
2018-02-21 00:15:59 +0100

    first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :cv, :download_pdf]
 
   def home
   end
+
+  def download_pdf
+  send_file(
+    "#{Rails.root}/public/propale.pdf",
+    filename: "propale.pdf",
+    type: "application/pdf"
+  )
+  render :home
+  end
+
+  def cv
+  end
+
 end
