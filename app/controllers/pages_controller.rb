d8c4f22fa645a801a56adbec5b772641194485f0
Henri <Henri@Henris-MacBook-Pro.local>
2018-02-22 15:36:22 +0100

    site complet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,40 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :cv, :download_pdf]
+  skip_before_action :authenticate_user!
 
   def home
   end
 
-  def download_pdf
-  send_file(
-    "#{Rails.root}/public/propale.pdf",
-    filename: "propale.pdf",
-    type: "application/pdf"
-  )
-  render :home
+  def bien
   end
 
-  def cv
+  def ville
+  end
+
+  def deplacer
+  end
+
+  def data
+  end
+
+  def data1
+  end
+
+  def data2
+  end
+
+  def data3
+  end
+
+  def data4
+  end
+
+  def data5
+  end
+
+  def data6
+  end
+
+  def data7
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "/download_pdf" => 'pages#download_pdf'
-  get "/cv" => "pages#cv"
+  get "/bien_vivre" => "pages#bien"
+  get "/rendre_la_ville" => "pages#ville"
+  get "/deplacer" => "pages#deplacer"
+  get "/data" => "pages#data"
+  get "/data1" => "pages#data1"
+  get "/data2" => "pages#data2"
+  get "/data3" => "pages#data3"
+  get "/data4" => "pages#data4"
+  get "/data5" => "pages#data5"
+  get "/data6" => "pages#data6"
+  get "/data7" => "pages#data7"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
